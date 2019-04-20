e9d856669d78543a0e0169ab0dcb7528bdb4b793
Brad Duck <brad.w.duck@gmail.com>
2019-04-20 13:28:57 -0400

    Updates

---
+++ precache-manifest.d596b32ebb0c9a8ca426f113c37e629c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4d6b219120dc330c652",
+    "url": "bwduck.github.io/duck-hangman/static/js/runtime~main.9cf69e37.js"
+  },
+  {
+    "revision": "cfb4cda3029052fcfbc5",
+    "url": "bwduck.github.io/duck-hangman/static/js/main.ab6def1a.chunk.js"
+  },
+  {
+    "revision": "af73d6773d1ee6a98067",
+    "url": "bwduck.github.io/duck-hangman/static/js/2.265db3a2.chunk.js"
+  },
+  {
+    "revision": "cfb4cda3029052fcfbc5",
+    "url": "bwduck.github.io/duck-hangman/static/css/main.ee6d8dbd.chunk.css"
+  },
+  {
+    "revision": "1edea00b7123b4f45a4f7ad33074368e",
+    "url": "bwduck.github.io/duck-hangman/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "bwduck.github.io/duck-hangman/precache-manifest.d596b32ebb0c9a8ca426f113c37e629c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("bwduck.github.io/duck-hangman/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
